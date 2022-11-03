90b60ece9936bf6904372849bbfb61027bfee418
Matt <1009003+tantaman@users.noreply.github.com>
2022-11-03 10:35:33 -0400

    same changes for `sqlite` package
    will share the code between both on the next commit

--- packages/crsqlite/build.sh
+++ packages/crsqlite/build.sh
@@ -4,34 +4,22 @@
 
 set -e
 
-# nix shell nixpkgs#esbuild
-
 rm -rf dist
 mkdir dist
 
 cp src/sqlite3.d.ts dist
 cp lib/sqlite3.wasm dist
 
-# cp lib/sqlite3.js lib/sqlite3.js.txt
-# pnpm esbuild src/sqlite3-wrapper.js --outfile=dist/sqlite3.js --format=esm --target=es2020 --bundle
-# rm lib/sqlite3.js.txt
 cat <(echo "export default function install(wrapper) {") lib/sqlite3.js <(echo "wrapper.self = self; }") > dist/sqlite3.js
 
 cp lib/sqlite3-opfs-async-proxy.js dist
 
 sed -i '' 's|const originalInit = self.sqlite3InitModule;|const originalInit = sqlite3InitModule;|g' dist/sqlite3.js
 
-# -w 0 disables newlines
-# PROXY_WORKER_B64=$(base64 -w 0 lib/sqlite3-opfs-async-proxy.js)
-# PROXY_WORKER_DATA_URL="data:text/javascript;base64,$PROXY_WORKER_B64"
-# sed -i '' "s|const W = new Worker(options.proxyUri);|const proxyWorkerUrl = new URL(\"$PROXY_WORKER_DATA_URL\", self.location);\n    const W = new Worker(proxyWorkerUrl, { type: 'module' });|g" dist/sqlite3.js
-
-# sed -i '' "s|const W = new Worker(options.proxyUri);|const W = new Worker(globalThis.proxyWorkerUrl, { type: 'module' });|g" dist/sqlite3.js
 sed -i '' "s|const W = new Worker(options.proxyUri);|const W = wrapper.makeProxyWorker();|g" dist/sqlite3.js
 
 sed -i '' "s|wasmBinaryFile = 'sqlite3.wasm';|wasmBinaryFile = wrapper.wasmBinaryFile;|g" dist/sqlite3.js
 
-# sqlite3.wasm file path is already fully resolved via Vite
 sed -i '' "s|wasmBinaryFile = locateFile(wasmBinaryFile)|// wasmBinaryFile = locateFile(wasmBinaryFile)|g" dist/sqlite3.js
 
 # prevent warn log message (even during dev)
--- packages/sqlite/src/sqlite3-wrapper.js
+++ packages/sqlite/src/sqlite3-wrapper.js
@@ -1,15 +1,16 @@
-import sqlite3RawSrc from '../lib/sqlite3.js.txt'
+import install from "./sqlite3.js";
 
+const wrapper = {};
 // these calls need to happen within a ESM module for Vite's magic to work
-const proxyWorkerUrl = new URL('sqlite3-opfs-async-proxy.js', import.meta.url).href;
-globalThis.makeProxyWorker = () => new Worker(proxyWorkerUrl, { type: 'module' });
+const proxyWorkerUrl = new URL("sqlite3-opfs-async-proxy.js", import.meta.url)
+  .href;
+wrapper.makeProxyWorker = () => new Worker(proxyWorkerUrl, { type: "module" });
 
-globalThis.wasmBinaryFile = new URL('sqlite3.wasm', import.meta.url).href;
+wrapper.wasmBinaryFile = new URL("sqlite3.wasm", import.meta.url).href;
 
 const init = () => {
-	eval(sqlite3RawSrc)
+  install(wrapper);
+  return wrapper.self.sqlite3InitModule(wrapper);
+};
 
-	return self.sqlite3InitModule()
-}
-
-export default init
+export default init;
