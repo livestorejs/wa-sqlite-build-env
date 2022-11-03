60cb5c21b0775a258604c4bd0cfa871f14113da5
Matt <1009003+tantaman@users.noreply.github.com>
2022-11-02 21:01:35 -0400

    construct and use the db (non worker)

--- packages/example/src/main.ts
+++ packages/example/src/main.ts
@@ -1,3 +1,19 @@
 import sqliteWasm from "sqlite-wasm-esm";
 
-console.log("hello");
+const sqlite = await sqliteWasm();
+
+const db = new sqlite.oo1.DB(":memory:");
+
+// @ts-ignore
+db.exec([
+  "CREATE TABLE foo (a primary key, b);",
+  "INSERT INTO foo VALUES (1, 2);",
+]);
+
+let rows = [];
+db.exec({
+  sql: "SELECT * FROM foo",
+  resultRows: rows,
+  rowMode: "object",
+});
+console.log(rows);
