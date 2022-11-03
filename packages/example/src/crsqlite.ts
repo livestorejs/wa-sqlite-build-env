b778159d4d92da6028153eaad527cc7958a200b3
Matt <1009003+tantaman@users.noreply.github.com>
2022-11-02 21:53:33 -0400

    run some crsqlite functions

--- packages/example/src/crsqlite.ts
+++ packages/example/src/crsqlite.ts
@@ -1,18 +1,26 @@
 import sqliteWasm from "crsqlite-wasm-esm";
+import { Uuid } from "uuid-tool";
 
 const sqlite = await sqliteWasm();
 
 const db = new sqlite.oo1.DB(":memory:");
 
-db.exec([
-  "CREATE TABLE foo (a primary key, b);",
-  "INSERT INTO foo VALUES (1, 2);",
-]);
-
+// @ts-ignore
+window.db = db;
 let rows = [];
+
+db.exec("CREATE TABLE foo (a primary key, b);");
+db.exec("INSERT INTO foo VALUES (1, 2);");
+db.exec("select crsql_dbversion();", { resultRows: rows });
+console.log("DB Version: ", rows[0][0]);
+rows = [];
+db.exec("select crsql_siteid();", { resultRows: rows });
+console.log("Site ID: ", new Uuid(rows[0][0]).toString());
+
+rows = [];
 db.exec({
   sql: "SELECT * FROM foo",
   resultRows: rows,
   rowMode: "object",
 });
-console.log(rows);
+console.log(rows[0]);
