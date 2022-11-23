3f43791ca274c8013e30268919579feddb59252b
Johannes Schickling <schickling.j@gmail.com>
2022-11-23 22:31:33 +0100

    fix vite build

--- packages/sqlite/nix/builder.sh
+++ packages/sqlite/nix/builder.sh
@@ -24,10 +24,10 @@ cat <(echo "export default function install(wrapper) {") $sqlitelib/sqlite3.js <
 sed -i "s|const originalInit = self.sqlite3InitModule;|const originalInit = sqlite3InitModule;|g" dist/sqlite3.js
 
 # Use `new URL()` pattern which can be understood by Vite
+sed -i "s|wasmBinaryFile = 'sqlite3.wasm';|wasmBinaryFile = new URL('sqlite3.wasm', import.meta.url).href;|g" dist/sqlite3.js
+sed -i "s|.uri = 'sqlite3.wasm'|.uri = new URL('sqlite3.wasm', import.meta.url).href|g" dist/sqlite3.js
 sed -i "s|const W = new Worker(options.proxyUri);|const W = new Worker(new URL(options.proxyUri, import.meta.url));|g" dist/sqlite3.js
-
-# Use `new URL()` pattern which can be understood by Vite
-sed -i "s|wasmBinaryFile = 'sqlite3.wasm';|wasmBinaryFile = new URL('sqlite3.wasm', import.meta.url).href|g" dist/sqlite3.js
+sed -i "s|\"sqlite3-opfs-async-proxy.js\";|new URL('sqlite3-opfs-async-proxy.js', import.meta.url).href;|g" dist/sqlite3.js
 
 # comment out as not neccessary with Vite
 sed -i "s|wasmBinaryFile = locateFile(wasmBinaryFile)|// wasmBinaryFile = locateFile(wasmBinaryFile)|g" dist/sqlite3.js
