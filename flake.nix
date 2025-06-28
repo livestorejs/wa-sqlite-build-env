fa58231c1f71ff25e56d6f76a34d12fee5182f6a
Johannes Schickling <schickling.j@gmail.com>
2025-06-28 16:26:43 +0200

    Update nixpkgs and flake configuration

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
     nixpkgsUnstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
